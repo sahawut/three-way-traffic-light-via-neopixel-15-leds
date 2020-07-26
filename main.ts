7b766facc955f7c00c30849fcacc343701620b85
sahawut <sahawut@gmail.com>
2020-07-26 10:19:56 -0400

    add is green variable

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    strip.clear()
-    yellow()
-    strip.show()
-    basic.pause(500)
-    strip.clear()
-    red()
-    strip.show()
+    if (is_green) {
+        strip.clear()
+        yellow()
+        strip.show()
+        basic.pause(500)
+        strip.clear()
+        red()
+        strip.show()
+    }
 })
 function green () {
+    is_green = 1
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
 }
 input.onButtonPressed(Button.A, function () {
@@ -21,12 +24,16 @@ input.onButtonPressed(Button.A, function () {
     strip.show()
 })
 function yellow () {
+    is_green = 0
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
 }
 function red () {
+    is_green = 0
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 }
 let strip: neopixel.Strip = null
+let is_green = 0
+is_green = 1
 radio.setGroup(1)
 strip = neopixel.create(DigitalPin.P1, 15, NeoPixelMode.RGB)
 strip.setBrightness(10)
