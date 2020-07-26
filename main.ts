e18ef3a0c5380d5270b57365f03c6008a19732f6
sahawut <sahawut@gmail.com>
2020-07-26 10:38:24 -0400

    add is red variable and works! (I could now be a police officer)

--- main.ts
+++ main.ts
@@ -10,30 +10,37 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 function green () {
+    is_red = 0
     is_green = 1
     strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
 }
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
-    strip.clear()
-    red()
-    strip.show()
-    basic.pause(500)
-    strip.clear()
-    green()
-    strip.show()
+    if (is_red) {
+        radio.sendNumber(1)
+        strip.clear()
+        red()
+        strip.show()
+        basic.pause(500)
+        strip.clear()
+        green()
+        strip.show()
+    }
 })
 function yellow () {
+    is_red = 0
     is_green = 0
     strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
 }
 function red () {
+    is_red = 1
     is_green = 0
     strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
 }
 let strip: neopixel.Strip = null
+let is_red = 0
 let is_green = 0
 is_green = 1
+is_red = 1
 radio.setGroup(1)
 strip = neopixel.create(DigitalPin.P1, 15, NeoPixelMode.RGB)
 strip.setBrightness(10)
